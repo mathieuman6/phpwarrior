80d4b3a2c72cfa5f56252737db0e5bb43bf82bb5
nathan71370 <nathanmercier@orange.fr>
2018-10-02 14:10:55 +0200

    Conversation_message

--- app/Models/ConversationMessage.php
+++ app/Models/ConversationMessage.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class ConversationMessage extends Model
 {
     protected $table = 'conversation_messages';
+
+    public function user() {
+        $this->belongsTo('App\Models\Users', 'user_id');
+    }
+
+    public function conversationMessage() {
+        $this->belongsTo('App\Models\Conversation', 'conversation_id');
+    }
 }
