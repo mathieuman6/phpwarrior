8f6b1e5c93f45df4b050373f02975f5a6c19b5dc
nathan71370 <nathanmercier@orange.fr>
2018-10-03 16:30:05 +0200

    fonctionne

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Conversation;
 use Illuminate\Http\Request;
 
 class ConversationController extends Controller
 {
-    public function index() {
-
+    public function index($conversation_id) {
+        $conversation = Conversation::find($conversation_id);
+        $messages = $conversation->conversationMessages;
+        return view('conversation', [
+            'messages' => $messages,
+        ]);
     }
 }
